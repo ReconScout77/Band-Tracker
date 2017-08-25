fc5a7be69bd33b634e55b9cdef93af6ee239e6d9
Robert Murray <alexmurray93@hotmail.com>
2017-08-25 14:58:22 -0700

    Add more routes and webpages.

--- BandTracker/Controllers/HomeController.cs
+++ BandTracker/Controllers/HomeController.cs
@@ -13,25 +13,88 @@ namespace BandTracker.Controllers
       return View();
     }
 
-    [HttpGet("/bandForm")]
+    [HttpGet("/bands")]
+    public ActionResult AllBands()
+    {
+      List<Band> allBands = Band.GetAll();
+      return View(allBands);
+    }
+
+    [HttpGet("/bands/bandForm")]
     public ActionResult BandForm()
     {
       return View();
     }
 
-    [HttpPost("/bandForm/new")]
+    [HttpPost("/bands/bandForm/new")]
     public ActionResult BandAdded()
     {
-      Band newBand = new Band(Request.Form("name"));
+      Band newBand = new Band(Request.Form["name"]);
       newBand.Save();
-      List<Band> allBands = Band.GetAll();
-      return View("AllBands", allBands);
+      return RedirectToAction("AllBands");
     }
 
-    [HttpGet("/venueForm")]
+    [HttpGet("/bands/{id}")]
+    public ActionResult BandDetails(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>{};
+      Band thisBand = Band.Find(id);
+      model.Add("band", thisBand);
+      model.Add("venues", thisBand.GetVenues());
+      return View(model);
+    }
+
+    [HttpGet("/venues")]
+    public ActionResult AllVenues()
+    {
+      List<Venue> allVenues = Venue.GetAll();
+      return View(allVenues);
+    }
+
+    [HttpGet("/venues/venueForm")]
     public ActionResult VenueForm()
     {
       return View();
+    }
+
+    [HttpPost("/venues/venueForm/new")]
+    public ActionResult VenueAdded()
+    {
+      Venue newVenue = new Venue(Request.Form["name"]);
+      newVenue.Save();
+      return RedirectToAction("AllVenues");
+    }
+
+    [HttpGet("/venues/{id}")]
+    public ActionResult VenueDetails(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>{};
+      Venue thisVenue = Venue.Find(id);
+      model.Add("venue", thisVenue);
+      model.Add("bands", thisVenue.GetBands());
+      return View(model);
+    }
+
+    [HttpGet("/venues/{id}/updateForm")]
+    public ActionResult VenueUpdateForm(int id)
+    {
+      Venue thisVenue = Venue.Find(id);
+      return View(thisVenue);
+    }
+
+    [HttpPost("/venues/{id}/updateForm/updated")]
+    public ActionResult UpdateVenue(int id)
+    {
+      Venue thisVenue = Venue.Find(id);
+      thisVenue.Update(Request.Form["updateName"]);
+      return RedirectToAction("VenueDetails");
+    }
+
+    [HttpGet("/venues/{id}/deleted")]
+    public ActionResult DeleteVenue(int id)
+    {
+      Venue.Find(id).Delete();
+      return RedirectToAction("AllVenues");
     }
   }
 }
